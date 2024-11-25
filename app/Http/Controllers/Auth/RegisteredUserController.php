2bfdab7e3f8db4dc44170094b47616eee14330d1
carolayn <juensiayala990@gmail.com>
2024-11-25 00:06:54 -0500

    Se arregló problema con laravel Breeze

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,6 +39,8 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'address' => $request->address,
+            'rol' => 'user'
         ]);
 
         event(new Registered($user));
